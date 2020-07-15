b30a7b8bab7b1112e4e68483a5ba3141ee78ef2c
shanguowei <1372054527@qq.com>
2020-07-15 17:35:02 +0800

    project structure setting

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,30 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
 import axios from "axios";
 import jsonp from "jsonp";
 
 export default {
   name: "App",
-  components: {
-    HelloWorld
-  },
-  data() {
-    return {
-      age: 30
-    };
-  },
+  components: {},
+  data() {},
   mounted() {
     // cors 跨域
     // let url =
     //   "https://www.easy-mock.com/mock/5b012c1fe6e1035843cd3aff/mockapi/table/list";
     // axios.get(url).then((res) => {
     //   console.log("res", res);
+    // })
+    // jsonp
+    // let url = "https://www.imooc.com/activity/servicetime";
+    // jsonp(url, (err, res) => {
+    //   let result = res;
+    //   this.data = result;
     // });
-
-    let url = "https://www.imooc.com/activity/servicetime";
-    jsonp(url, (err, res) => {
-      let result = res;
-      this.data = result;
-    });
   }
 };
 </script>
